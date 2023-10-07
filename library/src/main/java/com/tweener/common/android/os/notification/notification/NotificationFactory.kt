6bbbe0b33ad14f5b921468af715b5d68921f0161
Vivien Mahé <vivouneuh@gmail.com>
2023-10-07 10:46:45 +0200

    Creates basic set up for notification configuration

---
+++ library/src/main/java/com/tweener/common/android/os/notification/notification/NotificationFactory.kt
@@ -0,0 +1,37 @@
+package com.tweener.common.android.os.notification.notification
+
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.text.SpannableString
+import androidx.core.app.NotificationCompat
+
+/**
+ * @author Vivien Mahe
+ * @since 05/10/2023
+ */
+class NotificationFactory {
+
+    companion object {
+
+        fun create(
+            context: Context,
+            channelId: String,
+            title: String,
+            content: SpannableString,
+            smallIcon: Int,
+            largeIcon: Bitmap? = null,
+            pendingIntent: PendingIntent? = null,
+            autoCancel: Boolean = true
+        ): NotificationCompat.Builder =
+            NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(smallIcon)
+                .setLargeIcon(largeIcon)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(content))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true) // Automatically remove the notification when the user taps it
+    }
+}
